3131400ebf98c5ab6516bd0dd716a4160ea44fcb
Sam Dervis <sam@samdervis.com>
2026-03-11 11:00:18 +0100

    new release file

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
+  platform: "node",
+  target: "node20",
   dts: true,
   clean: true,
   sourcemap: true,
